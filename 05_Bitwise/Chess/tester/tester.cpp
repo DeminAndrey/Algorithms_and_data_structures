3c92468230ef25c11aa5fabec343bd506dc167c5
Андрей Демин <a.demin@innopolis.ru>
2025-04-11 17:35:01 +0300

    figure

--- 05_Bitwise/Chess/tester/tester.cpp
+++ 05_Bitwise/Chess/tester/tester.cpp
@@ -2,15 +2,12 @@
 #include "../chess.hpp"
 
 #include <iostream>
+#include <filesystem>
 #include <fstream>
 
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
 
-enum Figure {
-  King = 1,
-  Knigt = 2
-};
 
 Tester::Tester(int argc, char** argv) {
 	if (argc < 2) {
@@ -28,8 +25,12 @@ std::string Tester::out_file(int i) const {
   return m_test_path + "/test." + std::to_string(i) + ".out";
 }
 
-void Tester::run() {
+void Tester::all_run() {
   std::cout << "test path: " << m_test_path << "\n" << std::endl;
+
+  std::string stem = std::filesystem::path(m_test_path).stem().string();
+  int dir_number = std::atoi(stem.c_str());
+  m_figure = static_cast<Figure>(dir_number);
 
   int test_num = 0;
   while (true) {
@@ -54,16 +55,18 @@ void Tester::run() {
 	}
 }
 
-bool get_moves(Figure figure, int pos) {
+unsigned long long get_moves(int figure, int pos) {
   switch (figure) {
-  case Figure::King:
+  case Tester::Figure::KING:
     return Chess::get_king_moves(pos);
-  case Figure::Knigt:
+  case Tester::Figure::KNIGT:
     return Chess::get_knigt_moves(pos);
+  default:
+    exit(EXIT_FAILURE);
   }
 }
 
-bool run_test(const std::string& in, const std::string& out) {
+bool Tester::run_test(const std::string& in, const std::string& out) {
   int in_pos = std::stoi(in);
   std::vector<std::string> splitted;
   auto out_vec = boost::algorithm::split(splitted, out,
@@ -77,7 +80,7 @@ bool run_test(const std::string& in, const std::string& out) {
   int expected_moves_count = std::stoi(out_vec[0]);
   unsigned long long expected_moves_mask = std::stoull(out_vec[1]);
 
-  unsigned long long computed_moves_mask = Chess::get_king_moves(in_pos);
+  unsigned long long computed_moves_mask = get_moves(m_figure, in_pos);
   int computed_moves_count = BitCounter::counting_by_subtraction(computed_moves_mask);
 
   std::cout << "for position " << std::dec << in_pos << ", moves count: "
@@ -88,9 +91,19 @@ bool run_test(const std::string& in, const std::string& out) {
          expected_moves_mask == computed_moves_mask;
 }
 
+/*
+  Описание параметров командной строки
+  ./Chess <test_path>
+  Параметры:
+  <test_path>: Путь к папке с тестовыми данными
+
+  Примеры использования:
+  ./Chess C:/Chess/0.BITS/1.Bitboard
+*/
+
 int main(int argc, char** argv) {
   Tester tester(argc, argv);
-  tester.run();
+  tester.all_run();
 
   return EXIT_SUCCESS;
 }
--- 05_Bitwise/Chess/tester/tester.hpp
+++ 05_Bitwise/Chess/tester/tester.hpp
@@ -4,13 +4,19 @@
 
 class Tester {
 public:
+  enum Figure {
+    KING = 1,
+    KNIGT = 2
+  };
+
   Tester(int argc, char** argv);
-	void run();
+  void all_run();
+  bool run_test(const std::string& in,
+                const std::string& out);
 
 private:
 	std::string in_file(int i) const;
 	std::string out_file(int i) const;
   std::string m_test_path;
+  Figure m_figure;
 };
-
-bool run_test(const std::string& in, const std::string& out);
